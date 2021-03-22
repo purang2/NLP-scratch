36444ce4d648cb727b8e19f0811454e815140ba4
purang2 <purang2@nate.com>
2021-03-22 15:42:13 +0900

    commit at 3PM

---
+++ CBOW.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 14:29:41 2021
+
+@author: PC
+"""
+
+
+
+import sys
+sys.path.append('..')
+from util import preprocess, create_contexts_target, convert_one_hot
+
+text = 'You say goodbye and I say hello.'
+corpus, word_to_id, id_to_word = preprocess(text)
+
+contexts, target = create_contexts_target(corpus, window_size=1)
+
+vocab_size = len(word_to_id)
+target = convert_one_hot(target, vocab_size)
+contexts = convert_one_hot(contexts, vocab_size)
---
+++ simple_cbow.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 14:39:06 2021
+
+@author: PC
+"""
+
+import sys
+sys.path.append('..')
+import numpy as np
+from layers import MatMul, SoftmaxWithLoss
+
+class SimpleCBOW:
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+        
+        #가중치 초기화
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H,V).astype('f')
+        
+        #계층 생성
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+        
+        #모든 가중치와 기울기를 리스트에 모은다.
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.params += layer.grads
+            
+        #인스턴스 변수에 단어의 분산 표현을 저장한다.
+        self.word_vecs = W_in
+        
